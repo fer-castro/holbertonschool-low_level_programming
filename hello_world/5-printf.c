bf07e66ab8e5baefe0d0d9fcfdc09982f40b8098
root <6998@holbertonstudents.com>
2023-06-22 12:14:41 -0700

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line using the printf function
- * Description: A program that prints a line using the printf function
- * Return: Always 0 (Success)
- */
+* main - A program that prints a line using the printf function
+* Description: A program that prints a line using the printf function
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-printf("with propper grammar, but the outcome is a piece of art,\n")
+printf("with propper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
