9955bb2845ecf350893d7daa5a90f182fdd08f2d
root <6998@holbertonstudents.com>
2023-06-22 15:18:26 -0700

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - prints all single digit numbers
+* Description - prints all single digit numbers
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int num;
+for (num = 0; num 10; num++)
+putchar((num % 10) + '0');
+putchar ('\n');
+return (0);
+}
