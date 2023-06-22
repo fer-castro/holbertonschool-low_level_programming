e9b6c10812e244ef912a86c4964110582e19b959
root <6998@holbertonstudents.com>
2023-06-22 10:13:14 -0700

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -5,6 +5,6 @@
  */
 int main(void)
 {
-  printf("with propper grammar, but the outcome is a piece of art,\n")
-    return (0);
+printf("with propper grammar, but the outcome is a piece of art,\n")
+return (0);
 }
