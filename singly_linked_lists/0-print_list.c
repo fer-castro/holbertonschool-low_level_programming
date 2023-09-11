42abcada6e48fbb506011c1f2e520fefe4adb22c
fer-castro <6998@holbertonstudents.com>
2023-09-11 14:32:01 -0700

    commit

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,16 +1,9 @@
 #include <stdio.h>
 #include "lists.h"
 /**
-*
-*  * print_list - prints all the elements of a linked list
-*
-*   * @h: pointer to the list_t list to print
-*
-*    *
-*
-*     * Return: the number of nodes printed
-*
-*      
+* print_list - prints all the elements of a linked list
+* @h: pointer to the list_t list to print
+* Return: the number of nodes printed
 */
 size_t print_list(const list_t *h)
 {
