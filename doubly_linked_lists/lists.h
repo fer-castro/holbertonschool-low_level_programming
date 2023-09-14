8c060c6b5adbe199fea4bd10423a5571a507e081
fer-castro <6998@holbertonstudents.com>
2023-09-14 13:19:34 -0700

    commit

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -10,20 +10,21 @@
 */
 typedef struct dlistint_s
 {
-	int n;
-	struct dlistint_s *next;
-} dlistint_t;
+int n;
+struct dlistint_s *next;
+} 
+dlistint_t;
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
-dlistint_t *add_nodeint(dlistint_t **head, const int n);
-dlistint_t *add_nodeint_end(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 void free_dlistint(dlistint_t *head);
 void free_dlistint2(dlistint_t **head);
 int pop_dlistint(dlistint_t **head);
-dlistint_t *get_nodeint_at_index(dlistint_t *head, unsigned int index);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
-dlistint_t *insert_nodeint_at_index(dlistint_t **head, unsigned int idx, int n);
-int delete_nodeint_at_index(dlistint_t **head, unsigned int index);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int idx, int n);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 dlistint_t *reverse_dlistint(dlistint_t **head);
 size_t print_dlistint_safe(const dlistint_t *head);
 size_t free_dlistint_safe(dlistint_t **h);
