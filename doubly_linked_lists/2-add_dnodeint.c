d9adde3393c5493fd11a37c905e14acaa31ee30e
fer-castro <6998@holbertonstudents.com>
2023-09-14 13:02:08 -0700

    commit

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -7,12 +7,22 @@
 **/
 dlistint_t *add_nodeint(dlistint_t **head, const int n)
 {
-dlistint_t *new;
+dlistint_t *new = NULL;
 new = malloc(sizeof(dlistint_t));
 if (!new)
 return (NULL);
 new->n = n;
-new->next = *head;
+new->prev = NULL;
+if (*head == NULL)
+{
+new->next = NULL;
 *head = new;
+}
+else
+new->next = *head;
+if (*head)
+(*head)->prev = new;
+*head = new;
+}
 return (new);
 }
